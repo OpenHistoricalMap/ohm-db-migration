5269c150ed0b8def7b3f40fb9ee17014b3a47b8e
Rub21 <rub2106@gmail.com>
2024-05-03 14:31:35 -0500

    Update scripts

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-OSM_SEED_VERSION=0.1.0-n780.h5000b1e
+OSM_SEED_VERSION=0.1.0-n787.h2a19af9
 cat values.template.yaml | envsubst > values.yaml
 helm upgrade --install migration https://devseed.com/osm-seed-chart/osm-seed-${OSM_SEED_VERSION}.tgz -f values.yaml
